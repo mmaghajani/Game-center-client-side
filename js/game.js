39d4322fb817f4e60657eb79945892b453abe0f2
mahdi aghajani <mma137421@gmail.com>
2016-12-24 03:22:14 +0330

    Completes tabs panel

---
+++ js/game.js
@@ -0,0 +1,21 @@
+/**
+ * Created by mma on 12/22/16.
+ */
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+});
