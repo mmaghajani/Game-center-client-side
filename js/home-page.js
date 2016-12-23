04df402107b6a8da1179ebe51c219b66f9a324cf
mahdi aghajani <mma137421@gmail.com>
2016-12-23 15:05:01 +0330

    Completes new game viewport

--- js/home-page.js
+++ js/home-page.js
@@ -2,7 +2,62 @@
  * Created by mma on 12/22/16.
  */
 $(document).ready(function(){
-    var owl = $('.owl-carousel');
-    owl.owlCarousel({loop:true , items:3, dots:false });
+    var owl2 = $('#owl');
+    owl2.owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:false
+            },
+            1000:{
+                items:4,
+                loop:false
+            }
+        }
+    })
 
+    var owl1 = $('.owl-carousel');
+    owl1.owlCarousel({loop:true , dots:false ,responsiveClass:true ,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:false
+            },
+            1000:{
+                items:4,
+                loop:false
+            }
+        }});
+
+    // var owl2 = $('#owl');
+    // owl2.owlCarousel({
+    //     loop:true,
+    //     margin:10,
+    //     responsiveClass:true,
+    //     responsive:{
+    //         0:{
+    //             items:1,
+    //             nav:true
+    //         },
+    //         600:{
+    //             items:3,
+    //             nav:false
+    //         },
+    //         1000:{
+    //             items:4,
+    //             loop:false
+    //         }
+    //     }
+    // })
 });
