54d89c38ec0c5ac70f1f8cff6f395191c81c6825
mahdi aghajani <mma137421@gmail.com>
2016-12-24 13:48:16 +0330

    Completes same game panel

--- js/game.js
+++ js/game.js
@@ -2,19 +2,72 @@
  * Created by mma on 12/22/16.
  */
 $(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
+    var owl3 = $('#owl3');
+    owl3.owlCarousel({
         loop:true,
         margin:10,
-        nav:true,
+        responsiveClass:true,
         responsive:{
             0:{
-                items:1
+                items:1,
+                nav:true,
+                dots:false
             },
             600:{
-                items:3
+                items:3,
+                nav:false,
+                dots:false
             },
             1000:{
-                items:5
+                items:4,
+                loop:true,
+                dots:false
+            }
+        }
+    })
+    var owl2 = $('#owl2');
+    owl2.owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true,
+                dots:false
+            },
+            600:{
+                items:3,
+                nav:false,
+                dots:false
+            },
+            1000:{
+                items:4,
+                loop:true,
+                dots:false
+            }
+        }
+    })
+    var owl1 = $('#owl1');
+    owl1.owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true,
+                dots:false
+            },
+            600:{
+                items:3,
+                nav:false,
+                dots:false
+            },
+            1000:{
+                items:4,
+                loop:true,
+                dots:false
             }
         }
     })
