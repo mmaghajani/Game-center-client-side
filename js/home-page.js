29cf267c6932e826a96e3b7cab0bce3bb9d31359
mahdi aghajani <mma137421@gmail.com>
2016-12-25 17:32:09 +0330

    Adds ajax for slider one

--- js/home-page.js
+++ js/home-page.js
@@ -27,33 +27,28 @@ $(document).ready(function () {
     })
 
 
-
-    // $.ajaxSetup({
-    //     beforeSend: function(xhr) {
-    //         xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-    //     }
-    // });
-
-    // owl1.on('changed.owl.carousel', function (event) {
-    //     console.log(event.item.index)
-    //
-    // })
+    var requestURL = "http://localhost/home.json";
+    var titleOfActiveGame;
 
     $.ajax({
-        url: "http://localhost/home.json", type: 'GET', header:{'Access-Control-Allow-Origin' : '*'} , Origin: '*', success: function (data) {
+        url: requestURL , type: 'GET', headers:{'Access-Control-Allow-Origin' : '*'} , success: function (data) {
             if (data.response.ok == true) {
                 var gamesData = data.response.result.homepage;
                 var num = gamesData.slider.length;
                 var slider = document.getElementById('owl1');
                 console.log(slider)
                 for( var i = 0 ; i < num ; i++ ){
-                    var y = $('<div class="slider-item-one  text-right  vertical-align-wrap">' +
+                    var id = i;
+                    var y = $('<div class="slider-item-one  text-right  vertical-align-wrap"' + 'id="' + id + '">' +
                         '<p class="vertical-align text-info vertical-align--bottom h5"><strong>بررسی بازی ' +
                         (gamesData.slider[i]).title + '</strong></p>' +
                         '</div>').css('background' , 'url('+ (gamesData.slider[i]).small_image + ') no-repeat center center')
                     $('#owl1').append(y)
                 }
                 var owl1 = $('#owl1');
+                var numberOfItemsInSmall = 1;
+                var numberOfItemsInMedium = 3 ;
+                var numberOfItemsInLarge = 6 ;
                 owl1.owlCarousel({
                     loop: true, dots: false, responsiveClass: true, animateOut: 'fadeOut',
                     animateIn: 'fadeIn',
@@ -62,15 +57,15 @@ $(document).ready(function () {
                     autoplayHoverPause: true,
                     responsive: {
                         0: {
-                            items: 1,
+                            items: numberOfItemsInSmall,
                             nav: true
                         },
                         600: {
-                            items: 3,
+                            items: numberOfItemsInMedium,
                             nav: false
                         },
                         1000: {
-                            items: 6,
+                            items: numberOfItemsInLarge,
                             loop: true,
                             center: true
                         }
@@ -78,7 +73,29 @@ $(document).ready(function () {
                 });
 
                 owl1.on('changed.owl.carousel', function(event) {
-
+                    //
+                    // if(event.item)
+                    var s = document.getElementById('owl1')
+                    var g = s.getElementsByClassName('active')
+                    var id;
+                    if( g.length == numberOfItemsInLarge ){
+                        id = event.item.index - 6;
+                    }else if( g.length == numberOfItemsInMedium ){
+                        id = event.item.index - 3;
+                    }else {
+                        id = event.item.index - 2;
+                    }
+                    var sliderItem = document.getElementById(id);
+                    var child = sliderItem.childNodes;
+                    var title = child[0].textContent.substr(16)
+                    titleOfActiveGame = title;
+                    for( var i = 0 ; i < gamesData.slider.length ; i++){
+                        console.log((gamesData.slider[i]).title)
+                        if( "بازی " + title == (gamesData.slider[i]).title){
+                            $('#main-content').css({'background' :
+                            'url('+ (gamesData.slider[i]).large_image + ') no-repeat center center', 'background-size' : 'cover'})
+                        }
+                    }
                 })
             }
         }
